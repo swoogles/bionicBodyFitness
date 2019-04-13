8a53b75ff1dbe61d615bd510841201390ea27735
bfrasure <bill.frasure@gmail.com>
2019-04-13 14:30:48 -0600

    BBF-7 #time 25m #comment Cleanup some payment code

--- functions/stripe/test/TestPayments.go
+++ functions/stripe/test/TestPayments.go
@@ -8,20 +8,27 @@ import (
 )
 
 func executeStripePayment(stripePaymentToken string) string {
-	return executeStripePaymentWithAmount(stripePaymentToken, 999)
+	return executeTestStripePaymentWithAmount(stripePaymentToken, 999)
 }
 
-func executeStripePaymentWithAmount(stripePaymentToken string, amount int64) string {
-	stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
+func createTestPaymentFunction(testKey string) func(string, int64) string {
+	return func(stripePaymentToken string, amount int64) string {
+		stripe.Key = os.Getenv(testKey)
 
-	params := &stripe.ChargeParams{
-		Amount:      stripe.Int64(amount),
-		Currency:    stripe.String(string(stripe.CurrencyUSD)),
-		Description: stripe.String("Example charge"),
+		params := &stripe.ChargeParams{
+			Amount:      stripe.Int64(amount),
+			Currency:    stripe.String(string(stripe.CurrencyUSD)),
+			Description: stripe.String("Example charge"),
+		}
+		params.SetSource(stripePaymentToken)
+		ch, _ := charge.New(params)
+		fmt.Println(ch)
+
+		return "faux payment response"
 	}
-	params.SetSource(stripePaymentToken)
-	ch, _ := charge.New(params)
-	fmt.Println(ch)
 
-	return "faux payment response"
+}
+
+func executeTestStripePaymentWithAmount(stripePaymentToken string, amount int64) string {
+	return createTestPaymentFunction("STRIPE_SECRET_KEY")(stripePaymentToken, amount)
 }
