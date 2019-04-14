f3fad9f0a9ecf89ca4ab0e420421e3bd81b306c3
bfrasure <bill.frasure@gmail.com>
2019-04-14 16:24:51 -0600

    BBF-7 #time 25m #comment Maybe use right source for Customer

--- functions/stripe/test/AmazonInteractions.go
+++ functions/stripe/test/AmazonInteractions.go
@@ -12,16 +12,16 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	parameterMap, _ := url.ParseQuery(request.Body)
 	token := parameterMap.Get("stripeToken")
 
-	paymentResult := stripe.ExecuteTestStripePaymentWithAmount(token, 350)
+	//paymentResult := stripe.ExecuteTestStripePaymentWithAmount(token, 350)
 	productId := "prod_Et1gMmK1DWlq3S"
 	planId := stripe.CreateTestPlan(productId)
-	customerId := stripe.CreateTestCustomer()
-	stripe.CreateTestSubscription(planId, customerId)
+	customerId := stripe.CreateTestCustomer(token)
+	subscription := stripe.CreateTestSubscription(planId, customerId)
 	// TODO get some info about the charge, and then decide where to reroute
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       paymentResult,
+		Body:       "new subscription: " + subscription,
 	}, nil
 }
 
