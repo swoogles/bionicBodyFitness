2568adf63255eff6879ea96737fac4c4928611a0
bfrasure <bill.frasure@gmail.com>
2019-04-21 18:32:11 -0600

    BBF-35 #time 10m #comment Debugging statements

--- functions/stripe/test/TestNewMemberWithOrder.go
+++ functions/stripe/test/TestNewMemberWithOrder.go
@@ -14,18 +14,20 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	token := parameterMap.Get("stripeToken")
 	fmt.Println(stripe.JsonSerialize(parameterMap))
 
-	stripe.JsonSerialize(request)
+	fmt.Println(stripe.JsonSerialize(request))
 	//paymentResult := stripe.ExecuteTestStripePaymentWithAmount(token, 350)
 	//productId := "prod_Et1gMmK1DWlq3S"
 	//planId := stripe.CreateTestPlan(productId)
 	//customerId := stripe.CreateTestCustomer(token)
 	//subscription := stripe.CreateTestSubscription(planId, customerId)
+
+	// TODO stripe.CreateOrder()
 	result := stripe.ExecuteStripePaymentWithAmount(token, 123, "TEST_STRIPE_SECRET_KEY")
 	// TODO get some info about the charge, and then decide where to reroute
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "new subscription: " + result,
+		Body:       "new customer with order: " + result,
 	}, nil
 }
 
