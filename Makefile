f010b649a200d0305cd1c363ab29e896a13a7c0e
bfrasure <bill.frasure@gmail.com>
2019-04-29 19:53:23 -0600

    BBF-36 #time 25m #comment Attempt to lookup Stripe customer associated with Authenticated user

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ build:
 	go build -o lambdas/live-submit-stripe-charge  ./functions/stripe/live
 	go build -o lambdas/test-list-products  ./functions/stripe/products
 	go build -o lambdas/after-login  ./functions/identity
-	# Is this actually deploying?
+	go build -o lambdas/find-stripe-customer  ./functions/identity/find_customr
 	go build -o lambdas/register-new-member  ./functions/stripe/membership/new
 	# hugo server --buildDrafts --buildFuture --ignoreCache
 	hugo -v
---
+++ functions/identity/find_customer/FindCustomer.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	billStripe "github.com/swoogles/stripe"
+	"net/url"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+
+	parameterMap, _ := url.ParseQuery(request.Body)
+	email := parameterMap.Get("email")
+	if email == "" {
+		email = request.PathParameters["email"]
+	}
+
+	customer, err := billStripe.FindCustomer("TEST_STRIPE_SECRET_KEY", email)
+	if err != nil {
+		fmt.Println("Customer lookup error: " + err.Error())
+	} else {
+		fmt.Println(customer)
+	}
+
+	billStripe.JsonSerialize(request.PathParameters)
+	//m, _ := url.ParseQuery(request.Body)
+
+	fmt.Println("Request: ")
+	fmt.Println(billStripe.JsonSerialize(request))
+
+	// TODO get some info about the charge, and then decide where to reroute
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       billStripe.JsonSerialize(customer),
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
