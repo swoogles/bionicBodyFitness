2fa9391175221ad1eab83c2d502c1e437b806f3d
bfrasure <bill.frasure@gmail.com>
2019-04-29 20:30:40 -0600

    BBF-36 #time 15m #comment Try to attach real Stripe customer id to Identity user

--- functions/identity/find_customer/FindCustomer.go
+++ functions/identity/find_customer/FindCustomer.go
@@ -5,36 +5,18 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	billStripe "github.com/swoogles/stripe"
-	"net/url"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	email := request.QueryStringParameters["email"]
 
-	parameterMap, _ := url.ParseQuery(request.Body)
-	email := parameterMap.Get("email")
-	fmt.Println("Email: " + email)
-	if email == "" {
-		fmt.Println("Trying to get Pathparam")
-		email = request.PathParameters["email"]
-	}
-	if email == "" {
-		fmt.Println("Trying to get QueryString")
-		email = request.QueryStringParameters["email"]
-	}
-
-	fmt.Println("About to lookup Stripe customer. email: " + email)
 	customer, err := billStripe.FindCustomer("TEST_STRIPE_SECRET_KEY", email)
 	if err != nil {
 		fmt.Println("Customer lookup error: " + err.Error())
 	} else {
-		fmt.Println(customer)
+		fmt.Println(billStripe.JsonSerialize(customer))
 	}
 
-	billStripe.JsonSerialize(request.PathParameters)
-	//m, _ := url.ParseQuery(request.Body)
-
-	fmt.Println("Request: ")
-	fmt.Println(billStripe.JsonSerialize(request))
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       billStripe.JsonSerialize(customer),
@@ -44,6 +26,3 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 func main() {
 	lambda.Start(handler)
 }
-
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
