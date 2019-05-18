4bcc6dd18c1df6634b6afe97151cc225c5c2be8e
bfrasure <bill.frasure@gmail.com>
2019-05-17 21:06:26 -0600

    BBF-32 #time 0m #comment rename problematic "error" variable

--- functions/stripe/products/purchase/PurchaseProduct.go
+++ functions/stripe/products/purchase/PurchaseProduct.go
@@ -23,9 +23,9 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	existingCustomerid := parameterMap.Get("stripe_customer_id")
 	fmt.Println("existingCustomerid: " + existingCustomerid)
 	if existingCustomerid == "undefined" || len(existingCustomerid) == 0 {
-		customer, error := stripe.FindCustomer("STRIPE_SECRET_KEY", email)
-		if error != nil {
-			fmt.Println("Could not retrieve customer by email: " + error.Error())
+		customer, err := stripe.FindCustomer("STRIPE_SECRET_KEY", email)
+		if err != nil {
+			fmt.Println("Could not retrieve customer by email: " + err.Error())
 			customerId = stripe.CreateCustomer("STRIPE_SECRET_KEY", token, email, name)
 		} else {
 			customerId = customer.ID
